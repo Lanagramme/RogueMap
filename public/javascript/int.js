d85084f98381be321be349a1a246f6fa4ea46c77
Lanagramme <46745901+Lanagramme@users.noreply.github.com>
2021-08-16 08:55:44 -0400

    working

--- public/javascript/int.js
+++ public/javascript/int.js
@@ -1,263 +1,180 @@
+const pieces = [
+	/*0 */ { name: "nsew" },
+	/*1 */ { name: "nse" },
+	/*2 */ { name: "nsw" },
+	/*3 */ { name: "new" },
+	/*4 */ { name: "sew" },
+	/*5 */ { name: "nw" },
+	/*6 */ { name: "ne" },
+	/*7 */ { name: "se" },
+	/*8 */ { name: "sw" },
+	/*9 */ { name: "ns" },
+	/*10 */ { name: "ew" },
+	/*11 */ { name: "n" },
+	/*12 */ { name: "s" },
+	/*13 */ { name: "e" },
+	/*14 */ { name: "w" },
+	/*15 */ { name: "O" },
+	/*16 */ { name: "snew" },
+],
+grid = {},
+cl = console.log,
+oppose = { n: "s", s: "n", e: "w", w: "e" },
+sorties = origine => {
+	return pieces.filter(piece => piece.name.includes(oppose[origine]))
+},
+updateCase = (cooronnees, piece) => {
+	grid[cooronnees].name = piece.name
+}
 
-//tableaux des cases pouvant servir de sortie au point cardinal
-S=[ 0, 1, 2, 3, 5, 6, 9, 11 ]
-N=[ 0, 1, 2, 4, 7, 8, 9, 12 ]
-W=[ 0, 1, 3, 4, 5, 7, 10, 13 ]
-E=[ 0, 2, 3, 4, 6, 8, 10, 14 ]
+class grid_idem {
+constructor(data) {
+	;[this.name, this.x, this.y] = data
+}
+around() {
+	let res = []
+	for (let i of this.name) {
+		res.push(grid[this[i]])
+	}
 
-cl = console.log
-
-pieces = [
-/*0 */	{	name : 'nsew', exits : ['N','S','E','W']	}, 
-/*1 */	{	name : 'nse', exits : ['N','S','E']	}, 
-/*2 */	{	name : 'nsw', exits : ['N','S','W']	},
-/*3 */	{	name : 'new', exits : ['N','E','W']	},
-/*4 */	{	name : 'sew', exits : ['S','E','W']	},
-/*5 */	{	name : 'nw', exits : ['N','W']	},
-/*6 */	{	name : 'ne', exits : ['N','E']	},
-/*7 */	{	name : 'se', exits : ['S','E']	},
-/*8 */	{	name : 'sw', exits : ['S','W']	},
-/*9 */	{	name : 'ns', exits : ['N','S']	},
-/*10 */	{	name : 'ew', exits : ['E','W']	},
-/*11 */	{	name : 'n', exits : ['N']	},
-/*12 */	{	name : 's', exits : ['S']	},
-/*13 */	{	name : 'e', exits : ['E']	},
-/*14 */	{	name : 'w', exits : ['W']	},
-/*15 */	{	name : 'O', exits : []	}
-]
-
-var grid = {}
+	return res.filter(x => {
+		return x !== undefined
+	})
+}
+get coord() {
+	return `x${this.x}y${this.y}`
+}
+get n() {
+	return `x${this.x}y${this.y - 1}`
+}
+get s() {
+	return `x${this.x}y${this.y + 1}`
+}
+get e() {
+	return `x${this.x + 1}y${this.y}`
+}
+get w() {
+	return `x${this.x - 1}y${this.y}`
+}
+}
 
 // check for case exits
+function creator(nb_lignes, nb_colones) {
+for (i in grid) delete grid[i]
+$(".grid").html("")
+for (let ligne = 0; ligne < nb_lignes; ligne++) {
+	const ligne_actuelle = document.createElement("div")
+	ligne_actuelle.classList = "row"
 
-function creator(a,b){
-	plateau = document.getElementsByClassName('grid')[0]
-	plateau.innerHTML = ""
-	for (var i = 0; i < a; i++) {
-		row = document.createElement('div')
-		row.classList = 'row'
-		for (var j = 0; j < b; j++) {
-			caseData = { x:j, y:i , name:pieces[15].name, exits:pieces[15].exits }
-			Case = document.createElement('div')
-			Case.classList = `O`
-			Case.id = `x${caseData.x}y${caseData.y}`
-			grid[`x${caseData.x}y${caseData.y}`] = caseData
-			row.appendChild(Case)
-		}
-		plateau.appendChild(row)
+	for (let colone = 0; colone < nb_colones; colone++) {
+		const case_data = new grid_idem(["O", colone, ligne]),
+			html_case = document.createElement("div")
+		html_case.classList = `O`
+		html_case.id = case_data.coord
+		grid[case_data.coord] = case_data
+		ligne_actuelle.appendChild(html_case)
 	}
-	coor = `x${randomStart(b)}y${randomStart(a)}`
-	document.getElementById(coor).classList='nsew'
-	updateCase(coor, 0)
-	return coor
+	$(".grid").append(ligne_actuelle)
+}
+let coord_case = randomStart(nb_colones, nb_lignes)
+document.getElementById(coord_case).classList = "nsew"
+updateCase(coord_case, pieces[0])
+coord_case = randomStart(nb_colones, nb_lignes)
+document.getElementById(coord_case).classList = "snew"
+updateCase(coord_case, pieces[16])
+return coord_case
 }
 
-function randomStart(a){ 
-	start = Math.floor(Math.random() * a)
-	if (start == 0) return start+1
-	if (start ==a-1) return start -1
+function randomStart(nb_colones, nb_lignes) {
+randomCoord = MAX => {
+	start = Math.floor(Math.random() * MAX)
+	if (start == 0) return start + 1
+	if (start == MAX - 1) return start - 1
 	return start
 }
-
-function updateCase(coor,j=15){
-	cl(j)
-	grid[coor].name=pieces[j].name
-	grid[coor].exits=pieces[j].exits
+return `x${randomCoord(nb_colones)}y${randomCoord(nb_lignes)}`
 }
 
-function draw(foo){ //draw all exits of case foo
-	cl('%c draw :: ' + foo,'color: orange; font-weight: 900  ')
-	// cl(foo)
-	// debugger
-	for (i of grid[foo].exits) {
-		switch (i){
-			case 'N':
-				cl('%c N','color: red; font-weight: 900  ')
-				coor = `x${grid[foo].x}y${grid[foo].y-1}`
-				// si case visée est vide, verivier les réponses possibles parmis celles de la direction
-				if(grid[coor] != undefined && grid[coor].name == 'O'){
-					// cl('n')
-					possibles = caseEdit(coor,N)
-					//selectionner la solution au hasard parmis les possibles restants
-					solution = Math.floor(Math.random() * Math.floor(possibles.length))
-					// cl(possibles)
-					updateCase(coor, possibles[solution])
-				}
-				break
-			case 'S':
-				cl('%c S','color: red; font-weight: 900  ')
-				// cl('s')
-				coor = `x${grid[foo].x}y${grid[foo].y+1}`
-				if(grid[coor] != undefined && grid[coor].name == 'O'){
-					possibles = caseEdit(coor,S)
-					solution = Math.floor(Math.random() * Math.floor(possibles.length))
-					updateCase(coor, possibles[solution])
-					// cl(possibles)
-					// cl(possibles[solution])
-				}
-				break
-			case 'E':
-				cl('%c E','color: red; font-weight: 900  ')
-				// cl('e')
-				coor = `x${grid[foo].x+1}y${grid[foo].y}`
-				if(grid[coor] != undefined && grid[coor].name == 'O'){
-					possibles = caseEdit(coor,E)
-					solution = Math.floor(Math.random() * Math.floor(possibles.length))
-					updateCase(coor, possibles[solution])
-				}
-				break
-			case 'W':
-				cl('%c W','color: red; font-weight: 900  ')
-				// cl('w')
-				coor = `x${grid[foo].x-1}y${grid[foo].y}`
-				if(grid[coor] != undefined && grid[coor].name == 'O'){
-					possibles = caseEdit(coor,W)
-					solution = Math.floor(Math.random() * Math.floor(possibles.length))
-					updateCase(coor, possibles[solution])
-				}
-				break
-		}
+function draw(case_id) {
+cl("%c draw :: " + case_id, "color: orange; font-weight: 900  ")
+const case_active = grid[case_id]
+
+for (const direction of case_active.name.split("")) {
+	if (!grid.hasOwnProperty(case_active[direction])) continue
+
+	case_direction = grid[case_active[direction]]
+
+	if (case_direction.name == "O") {
+		const possibles = cases_possibles(case_direction.coord, direction),
+			options = sorties(direction)
+				.filter(x => {
+					for (i of possibles.impossibles) {
+						if (x.name.includes(i)) return
+					}
+					return x.name
+				})
+				.filter(x => {
+					for (i of possibles.oblicagoires) {
+						if (!x.name.includes(i)) return
+					}
+					return x.name
+				})
+		// cl(options)
+
+		//selectionner la solution au hasard parmis les possibles restants
+		solution = Math.floor(Math.random() * Math.floor(options.length))
+		// cl(options[solution])
+		updateCase(case_direction.coord, options[solution])
 	}
-	updatePlateau()
+}
+updatePlateau()
 }
 
-//pout chaque sortie de le case en cours
-
-function advance(){
+function advance() {
+do {
 	updatables = []
-	for (i in grid){
+	for (i in grid) {
 		i = grid[i]
-		if (i.name != "O") updatables.push(`x${i.x}y${i.y}`)
+		i.name != "O" &&
+			i.around().some(x => x.name == "O") &&
+			updatables.push(`x${i.x}y${i.y}`)
 	}
 	// cl(updatables)
-	for (j in updatables) {
-		draw(updatables[j])
-	}
+	for (j of updatables) draw(j, i.around())
+	cl(updatables.length)
+} while (updatables.length)
 }
 
+function cases_possibles(cible, origine) {
+cible = grid[cible]
+cl(`%c cases_possibles :: ${cible.coord}`, "color: orange; font-weight: 900")
 
-function caseEdit(cible,rep){
-	cl(`%c CaseEdit :: x${grid[cible].x}y${grid[cible].y}`,'color: orange; font-weight: 900  ')
-	Nord = grid[`x${grid[cible].x}y${grid[cible].y-1}`]
-	Sud  = grid[`x${grid[cible].x}y${grid[cible].y+1}`]
-	Est  = grid[`x${grid[cible].x+1}y${grid[cible].y}`]
-	West = grid[`x${grid[cible].x-1}y${grid[cible].y}`]
-	// valides=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-	sorties=[]
-	solutions=[]
-	
-	valides = []
-	for (i of rep) valides[i] = rep[i]
+const options = { impossibles: [], oblicagoires: [] }
 
-	// if((Nord == undefined) || (Nord.name != 'O' && !Nord.name.includes('s')))
-	// 	// for (i of S) {if (valides.includes(i)) valides.splice(valides.indexOf(i), 1)}
-	// 	{ for (i of valides) if (pieces[i].name.includes('n')) valides.splice(valides.indexOf(i), 1) }
-	// else {
-	// 	if (Nord.name != 'O' && Nord.name.includes('s')) /*sorties.push('n')*/
-	// 		{for (i of valides)	if (!pieces[i].name.includes('n')) valides.splice(valides.indexOf(i), 1)}
-	// }
-	// if((Sud == undefined) || (Sud.name != 'O' && !Sud.name.includes('n')))
-	// 	// for (i of N) {if (valides.includes(i)) valides.splice(valides.indexOf(i), 1)}
-	// 	{for (i of valides) if (pieces[i].name.includes('s')) valides.splice(valides.indexOf(i), 1)}
-	// else if (Sud.name != 'O' && Sud.name.includes('n')) /*sorties.push('s')*/ 
-	// 	{for (i of valides) if (!pieces[i].name.includes('s')) valides.splice(valides.indexOf(i), 1)}
-
-	// if((Est == undefined) ||  (Est.name != 'O' && !Est.name.includes('w')))
-	// 	// for (i of W) {if (valides.includes(i)) valides.splice(valides.indexOf(i), 1)}
-	// 	{for (i of valides) if (pieces[i].name.includes('e')) valides.splice(valides.indexOf(i), 1)}
-	// else if (Est.name != 'O' && Est.name.includes('w')) /*sorties.push('e')*/ 
-	// 	{for (i of valides) if (!pieces[i].name.includes('e')) valides.splice(valides.indexOf(i), 1)}
-
-	// if((West == undefined) || (West.name != 'O' && !West.name.includes('e')))
-	// 	// for (i of E) {if (valides.includes(i)) valides.splice(valides.indexOf(i), 1)} 
-	// 	{for (i of valides) if (pieces[i].name.includes('w')) valides.splice(valides.indexOf(i), 1)}
-	// else if (West.name != 'O' && West.name.includes('e')) /*sorties.push('w')*/ 
-	// 	{for (i of valides) if (!pieces[i].name.includes('w')) valides.splice(valides.indexOf(i), 1)}
-
-		// if((Nord == undefined) || (Nord.name != 'O' && !Nord.name.includes('s')))
-		// 	{for (i of valides) if (pieces[i].name.includes('n')) valides.splice(valides.indexOf(i), 1)}
-		// else if (Nord.name != 'O' && Nord.name.includes('s'))
-		// 	{for (i of valides)	if (!pieces[i].name.includes('n')) valides.splice(valides.indexOf(i), 1)}
-
-		// function checks(Card, C, nC, valides){
-		// 	if((Card == undefined) || (Card.name != 'O' && !Card.name.includes('nC')))
-		// 		{for (i of valides) if (pieces[i].name.includes('C')) valides.splice(valides.indexOf(i), 1)}
-		// 	else if (Card.name != 'O' && Card.name.includes('nC'))
-		// 		{for (i of valides)	if (!pieces[i].name.includes('C')) valides.splice(valides.indexOf(i), 1)}
-
-		// 	return valides
-		// }
-		function checks(Card, C, nC, valides){
-			if((Card == undefined) || (Card.name != 'O' && !Card.name.includes(nC)))
-				{
-					for (var i = valides.length - 1; i >= 0; i--)
-					{
-						if (valides[i] == 'k ') continue
-						cl(i)
-						if (pieces[i].name.includes(C)) valides.splice(valides.indexOf(i), 1)
-					}
-				}
-			else if (Card.name != 'O' && Card.name.includes(nC))
-				{
-					for (var i = valides.length - 1; i >= 0; i--)
-					{
-						if (valides[i] == 'k ') continue
-						if (!pieces[i].name.includes(C)) valides.splice(valides.indexOf(i), 1)
-
-					}
-						
-				}
-
-			return valides
-		}
-
-		valides = checks(Nord,'n', 's', valides)
-		valides = checks(Sud,'s', 'n', valides)
-		valides = checks(Est,'e', 'w', valides)
-		valides = checks(West,'w', 'e', valides)
-
-	// for (x of valides) solutions.push(x)
-	
-
-	
-	// for (j of sorties) {
-	// 	for (let k = valides.length-1; k >-1; k--) {
-	// 		const element = valides[k];
-	// 		a = pieces[element].name
-	// 		if (!a.includes(j)) {
-	// 			cl(valides)
-	// 			cl(element)
-	// 			valides.splice(valides.indexOf(element),1)
-	// 			cl(valides)
-	// 			cl('--------------------')
-	// 		}
-	// 	}
-	// }
-
-	// // Nvalides = JSON.stringify(valides)
-	// // Nsolutions = JSON.stringify(solutions)
-	// // cl({Nvalides, Nsolutions})
-	cl(valides);
-	return valides
+for (i of ["n", "s", "e", "w"]) {
+	if (grid.hasOwnProperty(cible[i])) {
+		if (
+			grid[cible[i]].name != "O" &&
+			!grid[cible[i]].name.includes(oppose[i])
+		)
+			options.impossibles.push(i)
+		else if (grid[cible[i]].name.includes(oppose[i]))
+			options.oblicagoires.push(i)
+	} else options.impossibles.push(i)
+}
+return options
 }
 
-
-
-
-function updatePlateau(){
-	for (i in grid){
-		i = grid[i]
-		a=document.getElementById(`x${i.x}y${i.y}`)
-		a.classList = i.name
-	}
+function updatePlateau() {
+for (i in grid) {
+	i = grid[i]
+	a = document.getElementById(i.coord)
+	cl(i.coord)
+	a.classList = i.name
+}
 }
 
-
-
-function toadvance(){
-	cl('%c ======== ======== ========','color: red; font-weight: 900  ')
-	advance()
+function toadvance() {
+cl("%c ======== ======== ========", "color: red; font-weight: 900  ")
+advance()
 }
-
-// draw(creator(6,9))
-
